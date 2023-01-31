29d06511c622ac76eb1a73220b06920938ad5c9e
Scott Meadows <Scottmeadows04@gmail.com>
2023-01-31 15:58:01 -0500

    refactor friendship serializer, fix serializer security issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,40 @@ class User < ApplicationRecord
       with: /\A[a-zA-Z0-9]+([-._][a-zA-Z0-9]+)*@[a-zA-Z0-9-]+\.\w{2,}\z/,
       message: 'email must follow standard format, i.e. test@example.com'
     }
+
+  def mutual_friends(limit = 10)
+    friendships.where(pending: false).limit(limit).map(&:friend)
+  end
+
+  def outgoing_friends(limit = 10)
+    friendships.where(pending: true).limit(limit).map(&:friend)
+  end
+
+  def incoming_friends(limit = 10)
+    Friendship.where(friend_id: id, pending: true).limit(limit).map(&:user)
+  end
+
+  def friendship_with(current_user_id)
+    # current_user = User.find_by(id: current_user_id)
+    friendship =
+      friendships.where(friend_id: current_user_id).or(Friendship.where(friend_id: id, user_id: current_user_id)).first
+    friendship_status(friendship)
+  end
+
+  private
+
+  def friendship_status(friendship)
+    if friendship
+      status = if !friendship.pending
+                 'friends'
+               elsif friendship.user_id == id
+                 'pending-incoming'
+               else
+                 'pending-outgoing'
+               end
+      { id: friendship.id, status: status }
+    else
+      { id: nil, status: false }
+    end
+  end
 end
--- app/serializers/verbose_user_serializer.rb
+++ app/serializers/verbose_user_serializer.rb
@@ -2,36 +2,32 @@ class VerboseUserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name, :email, :zip_code, :friends, :outgoing_friends, :incoming_friends, :friendship
 
   def friends
-    object.friendships.where(pending: false).limit(10).map(&:friend)
+    serialize_each(object.mutual_friends, UserSerializer)
   end
 
   def outgoing_friends
-    return nil unless instance_options[:logged_in_as] == object.id
+    return nil unless authorize_current
 
-    object.friendships.where(pending: true).limit(10).map(&:friend)
+    serialize_each(object.outgoing_friends, UserSerializer)
   end
 
   def incoming_friends
-    return nil unless instance_options[:logged_in_as] == object.id
+    return nil unless authorize_current
 
-    Friendship.where(friend_id: object.id, pending: true).limit(10).map(&:user)
+    serialize_each(object.incoming_friends, UserSerializer)
   end
 
   def friendship
-    friendship = (object.friendships.where(friend_id: instance_options[:logged_in_as])
-                        .or(Friendship.where(friend_id: object.id, user_id: instance_options[:logged_in_as])).first)
+    object.friendship_with(instance_options[:logged_in_as])
+  end
 
-    if friendship
-      status = if !friendship.pending
-                 'friends'
-               elsif friendship.user_id == object.id
-                 'pending-incoming'
-               else
-                 'pending-outgoing'
-               end
-      { id: friendship.id, status: status }
-    else
-      { id: nil, status: false }
-    end
+  private
+
+  def authorize_current
+    instance_options[:logged_in_as] == object.id
+  end
+
+  def serialize_each(data, serializer)
+    ActiveModelSerializers::SerializableResource.new(data, each_serializer: serializer)
   end
 end
