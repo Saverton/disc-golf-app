0750a2aabe193fb8eadaf833e9f4ddcc53f113e0
Scott Meadows <Scottmeadows04@gmail.com>
2023-02-07 13:02:08 -0500

    prepare for deploy with AWS creds

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   has_many :posts
   has_many :likes, as: :likable
 
-  has_one_attached :image, dependent: :destroy
+  has_one_attached :image
 
   validates_presence_of :name, :address
   validates :name, uniqueness: {
@@ -10,8 +10,20 @@ class Course < ApplicationRecord
   }
   validates :description, length: { minimum: 10 }
   validates :num_holes, numericality: { greater_than_or_equal_to: 1 }
+  validate :acceptable_image
 
   def image_url
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
+  end
+
+  private
+
+  def acceptable_image
+    return unless image.attached?
+
+    acceptable_types = ['image/jpeg', 'image/png']
+    return unless acceptable_types.include?(image.content_type)
+
+    errors.add(:image, 'must be a JPEG or PNG')
   end
 end
