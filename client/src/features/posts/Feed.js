c1f7fad25b2eb995f1d95dc5b4d7bd487d69a649
Scott Meadows <Scottmeadows04@gmail.com>
2023-02-13 10:18:42 -0500

    prevent load more posts on profiles

--- client/src/features/posts/Feed.js
+++ client/src/features/posts/Feed.js
@@ -1,11 +1,16 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchPosts } from './postsSlice';
-import { Grid, Header } from 'semantic-ui-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts, loadMorePosts } from './postsSlice';
+import { Grid, Header, Message, Button } from 'semantic-ui-react';
 import PostList from './PostList';
 
 export default function Feed() {
   const dispatch = useDispatch();
+  const errors = useSelector(state => state.posts.errors);
+
+  function onLoadClick() {
+    dispatch(loadMorePosts());
+  }
 
   useEffect(() => {
     dispatch(fetchPosts());
@@ -14,7 +19,15 @@ export default function Feed() {
   return (
     <Grid.Column width={10}>
       <Header size="large" dividing>Recent Posts</Header>
+
       <PostList />
+
+      <Header textAlign='center'>
+        <Message hidden={errors.length === 0} error>
+          <Message.Header>{errors.join(', ')}</Message.Header>
+        </Message>
+        <Button onClick={onLoadClick}>Load More...</Button>
+      </Header>
     </Grid.Column>
   );
 }
--- client/src/features/posts/PostList.js
+++ client/src/features/posts/PostList.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { loadMorePosts } from './postsSlice';
+import { useSelector } from 'react-redux';
 import Post from './Post';
 import PostPlaceholders from '../../components/PostPlaceholders';
-import { Feed, Header, Icon, Button, Message } from 'semantic-ui-react';
+import { Feed, Header, Icon} from 'semantic-ui-react';
 
 export default function PostList() {
   const { entities: posts, loading, errors } = useSelector(state => state.posts);
-  const dispatch = useDispatch();
-
-  function onLoadClick() {
-    // Dispatch load function
-    dispatch(loadMorePosts());
-  }
 
   const postsList = posts.map((p, idx) => <Post key={`post-${idx}`} post={p} index={idx} />);
   
@@ -28,16 +21,8 @@ export default function PostList() {
     );
 
   return (
-    <>
-      <Feed>
-        {postsList}
-      </Feed>
-      <Header textAlign='center'>
-        <Message hidden={errors.length === 0} error>
-          <Message.Header>{errors.join(', ')}</Message.Header>
-        </Message>
-        <Button onClick={onLoadClick}>Load More...</Button>
-      </Header>
-    </>
+    <Feed>
+      {postsList}
+    </Feed>
   );
 }
